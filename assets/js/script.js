4d694651f57bd9b27f1903e1c8bc03fdd4eb1db2
Azarill <heierms@gmail.com>
2022-04-22 02:46:22 -0500

    added local storage save

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ var setCurrentDate = function(){
     var month = moment().format("dddd, MMMM Do");
     var currentHour = moment().hour();
 
-    console.log(currentHour);
+   
     //selects current day ID and inserts text into HTML
     $("#currentDay").text(month);
 
@@ -18,13 +18,22 @@ var setCurrentDate = function(){
      //Loop through times to check if they are past current
     for(var i= 0; i < hours.length; i++){
          var timeBlockArr = hours[i].attributes[1].value;
+         
         
          
-         //check current time vs time block
+        // Apply color background based on time.
          if(moment().hour() < timeBlockArr){
              //select parentElement and apply a class
-             
-             //hours[i].parentElement.attributes.class.value = "row past";
+             hours[i].parentElement.nextElementSibling.classList.value ="col-9 h-100 padding future";
+            
+         }
+         else if(moment().hour() == timeBlockArr){
+            hours[i].parentElement.nextElementSibling.classList.value ="col-9 h-100 padding present";
+
+
+         }else{
+            hours[i].parentElement.nextElementSibling.classList.value ="col-9 h-100 padding past";
+
          }
 
      }
@@ -32,5 +41,22 @@ var setCurrentDate = function(){
     
     
 }
+
+$(".target").click(function(event){
+    var event = event.target;
+    var saveBtn = "saveBtn width-100 h-100 align-left";
+    var lock = $("span[class=material-symbols-outline]");
+    var eventTarget = event.classList.value;
+    console.log(eventTarget)
+    
+    if(eventTarget === saveBtn){
+        var testVar = event.$("textarea:first-of-type");
+        console.log(testVar)
+        var textAreaText = event.parentElement.parentElement.children[1].children.textArea.value;
+        if(textAreaText){
+            localStorage.setItem("information",JSON.stringify(textAreaText));
+        }
+    }
+})
 
 setCurrentDate();
